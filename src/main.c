e088cc3ca2ff977e41aa0d1a6b4e13853952390d
Mokhtar-dev <generalmk122@gmail.com>
2026-04-10 10:18:19 -0700

    Made a test in the Main function to see if the function that checks if the game is over works, and it does. I also added a function to print the board, which will be useful for testing and debugging. I also made some changes to the utils functions, but I'm not sure if they are correct yet. I will test them later.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-int main() {
-    printf("Hello, World!\n");
+#include "board.h"
+
+int main(void) {
+    board_init();
+
+    Board b;
+    board_start(&b);
+    board_print(&b);
+
+    /* test a custom position */
+    board_from_fen(&b,"r1bqkb1r/pppp1ppp/2n2n2/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 4 4");
+    board_print(&b);
+
     return 0;
 }
